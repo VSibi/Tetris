4c1cd6462a0b4c4be5db933150d76e1b51fdce92
VSibi <win5783@gmail.com>
2017-04-09 23:44:18 +0300

    fixed

---
+++ app/src/main/java/com/sibich/tetris/HelpOpenImageModeFragment.java
@@ -0,0 +1,20 @@
+package com.sibich.tetris;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Slavon on 21.03.2017.
+ */
+public class HelpOpenImageModeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_help_open_image_mode, parent, false);
+    }
+}
